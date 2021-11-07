e1a67cbd07c49ea5b45a6b2a4352efabe4f07417
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-07 16:39:43 -0600

    commit tree 5e9b8466d53232882669d7c7a574e22ad3cbf2b8

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -22,6 +22,7 @@ class StudentsController < ApplicationController
 
   def index
     @students = Student.search(params[:query])
+    # byebug
     render 'index'
   end
 
--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,12 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query)
+    if query.present?
+      self.where("name LIKE ?", "%#{query}%")
+    else
+      Student.all
+    end
+  end
 end
